a8a2e2ee773686b5b42034730887f28fcd30e051
jarviszhang <yichen_zhang_@outlook.com>
2025-09-15 17:05:04 -0400

    init

---
+++ download_weights.py
@@ -0,0 +1,26 @@
+from huggingface_hub import snapshot_download, login
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+import sys
+
+# set project root
+PROJECT_ROOT = Path(__file__).parent
+MODEL_DIR = PROJECT_ROOT / "checkpoints" / "FastVLM-7B"
+sys.path.append(str(PROJECT_ROOT))
+
+# load environment variables from .env file
+load_dotenv()
+HF_token = os.getenv("HF_TOKEN")
+
+# login to huggingface
+login(token=HF_token)
+#   download the model
+snapshot_download(
+    repo_id="apple/FastVLM-7B",
+    local_dir=str(MODEL_DIR),
+    local_dir_use_symlinks=False 
+)
+
+
+
